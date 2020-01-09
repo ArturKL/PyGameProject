693604252052e75bcdc38c8b12e24afe76917689
ArturKL <arturkalimullin10@gmail.com>
2020-01-09 19:54:58 +0300

    Добавлены спрайты Класс Crystal

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pygame
-from random import choice
+import random
+import os
 pygame.init()
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
+SIZE = WIDTH, HEIGHT = 1137, 910
+screen = pygame.display.set_mode(SIZE)
+CELL_SIZE = 100
+WHITE = pygame.Color('white')
+BOARD_COLOR = (13, 127, 184)
+
+
+def load_image(name):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert_alpha()
+    return image
 
 
 class Board:
@@ -10,14 +20,19 @@ class Board:
         self.width = width
         self.height = height
         self.board = [[] for i in range(width)]
-        print(self.board)
         for i in range(width):
             for _ in range(height):
-                self.board[i].append(choice([0, 1]))
+                self.board[i].append(random.choice([0, 1]))
         self.left = 10
         self.top = 10
         self.cell_size = 30
         self.turn = 1
+        Crystal(1, (0, 0))
+        Crystal(2, (1, 0))
+        Crystal(3, (2, 0))
+        Crystal(4, (3, 0))
+        Crystal(5, (4, 0))
+        Crystal(6, (5, 0))
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -33,7 +48,7 @@ class Board:
                     if x <= check[0] <= x + self.cell_size and y <= check[1] <= y + self.cell_size:
                         return i, j
                 rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
-                pygame.draw.rect(screen, pygame.Color('white'), rect, 1)
+                pygame.draw.rect(screen, BOARD_COLOR, rect, 1)
 
     def get_click(self, pos):
         cell = self.get_cell(pos)
@@ -47,15 +62,43 @@ class Board:
         pass
 
 
-board = Board(7, 7)
-board.set_view(25, 25, 65)
+c_images = {1: load_image('1.png'),
+            2: load_image('2.png'),
+            3: load_image('3.png'),
+            4: load_image('4.png'),
+            5: load_image('5.png'),
+            6: load_image('6.png')}
+c_sprites = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+
+
+class BackGround(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = load_image('background.png')
+        self.image = pygame.transform.scale(self.image, SIZE)
+        self.rect = self.image.get_rect()
+
+
+class Crystal(pygame.sprite.Sprite):
+    def __init__(self, c_type, pos):
+        super().__init__(c_sprites)
+        self.image = c_images[c_type]
+        self.image = pygame.transform.scale(self.image, (CELL_SIZE, CELL_SIZE))
+        self.rect = self.image.get_rect().move(pos[0] * CELL_SIZE + 5, pos[1] * CELL_SIZE + 5)
+
+
+board = Board(9, 9)
+board.set_view(5, 5, CELL_SIZE)
 running = True
+BackGround()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
             board.get_click(event.pos)
+    all_sprites.draw(screen)
     board.render()
+    c_sprites.draw(screen)
     pygame.display.flip()
-    screen.fill((0, 0, 0))
